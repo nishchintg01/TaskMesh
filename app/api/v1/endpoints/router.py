4ff040f29b12b8cf9a2ee8d3392284bf38e86a9a
nishchintg01 <nishchintg01@gmail.com>
2026-07-16 19:57:07 +0530

    Added Common Routing File, Updated Heath Endpoint Reponse with Pydantic Response Model

---
+++ app/api/v1/endpoints/router.py
@@ -0,0 +1,9 @@
+"""
+API Router Module
+"""
+from fastapi import APIRouter
+from app.api.v1.endpoints.health import router as health_router
+
+router = APIRouter()
+
+router.include_router(health_router)
